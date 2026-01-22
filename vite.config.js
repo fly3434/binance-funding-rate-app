074b72c2dd538789c8f85fcf1ad0d5dd061ababf
s70634@gmail.com <s70634@gmail.com>
2026-01-23 00:43:55 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/binance-funding-rate-app/', 
+  base: '/', 
   plugins: [react()],
 })
